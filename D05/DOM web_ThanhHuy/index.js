20ce9b323cdc32ad136f334b9b2296ae6a62bce0
Bui Thanh Huy <huy.bt8686@sinhvien.hoasen.edu.vn>
2022-02-12 10:03:50 +0700

    nop bai

--- D05/DOM web_ThanhHuy/index.js
+++ D05/DOM web_ThanhHuy/index.js
@@ -1,29 +1,43 @@
-let games = [{name: "BattleField", price: 100000, photoUrl:"./images/g1.png"},
-{name: "League of Legends", price: 10000, photoUrl:"./images/g2.jpg"},
-{name: "Valorant", price: 50000, photoUrl:"./images/g3.jpg"},
-{name: "God of War", price: 1000, photoUrl:"./images/g4.jpg"},
-{name: "Nekopara", price: 53000, photoUrl:"./images/g5.jpg"},
-{name: "Free Fire", price: 100, photoUrl:"./images/g6.jpg"},
-{name: "Genshin Impact", price: 11100, photoUrl:"./images/g7.jpg"}];
+let games = [
+  { name: "BattleField", price: "100000 VNĐ", photoUrl: "./images/g1.png" },
+  {
+    name: "League of Legends",
+    price: "10000 VNĐ",
+    photoUrl: "./images/g2.jpg",
+  },
+  { name: "Valorant", price: "50000 VNĐ", photoUrl: "./images/g3.jpg" },
+  { name: "God of War", price: "1000 VNĐ", photoUrl: "./images/g4.jpg" },
+  { name: "PES 2021", price: "53000 VNĐ", photoUrl: "./images/g5.png" },
+  { name: "Free Fire", price: "1000 VNĐ", photoUrl: "./images/g6.jpg" },
+  { name: "Genshin Impact", price: "11100 VNĐ", photoUrl: "./images/g7.jpg" },
+  {
+    name: "One Piece Pirate Warrior 3",
+    price: "150000 VNĐ",
+    photoUrl: "./images/g18.png",
+  },
+  {
+    name: "Captain Tsubasa: Rise of New Champion",
+    price: "17000 VNĐ",
+    photoUrl: "./images/g19.png",
+  },
+  { name: "Naruto Storm 4", price: "13000 VNĐ", photoUrl: "./images/g20.png" },
+];
 
-let group = document.getElementsByClassName('items');
+let group = document.getElementsByClassName("items");
 
+for (let i = 0; i < games.length; i++) {
+  let temp = document.createElement("div");
+  temp.className = "item";
+  let temp2 = document.createElement("img");
+  temp2.className = "img";
+  let temp3 = document.createElement("h3");
+  let temp4 = document.createElement("h4");
+  temp2.src = games[i].photoUrl;
+  temp3.innerHTML = games[i].name;
+  temp4.innerHTML = games[i].price;
+  temp.appendChild(temp2);
+  temp.appendChild(temp3);
+  temp.appendChild(temp4);
 
-for(let i = 0; i < games.length; i++) {
-
-    let temp = document.createElement('div');
-    temp.className="item"
-    let temp2 = document.createElement('img');
-    temp2.className="img";
-    let temp3 = document.createElement('h3');
-    let temp4 = document.createElement('h4');
-    temp2.src=games[i].photoUrl;
-    temp3.innerHTML=games[i].name;
-    temp4.innerHTML=games[i].price;
-    temp.appendChild(temp2); 
-    temp.appendChild(temp3);
-    temp.appendChild(temp4);
-
-
-    group[0].appendChild(temp);
+  group[0].appendChild(temp);
 }
