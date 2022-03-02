a699008c841ea24ab278bd396f6c769e93aceb2a
hien.hv6193 <hien.hv6193@sinhvien.hoasen.edu.vn>
2022-03-02 21:53:22 +0700

    server-backend

--- D022/Server-VanHien/src/server/index.js
+++ D022/Server-VanHien/src/server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app =express();
 const bodyParser = require('body-parser');
+const path = require("path");
 
 app.use(bodyParser.json());
 let arr =[{name:"Hiền"},{name:"Huân"},{name:"Thiên"}];
 app.listen('3000',(request,response)=>{
     console.log("Server đã chạy");
+})
+
+app.get("/",(request,response)=>{
+    console.log(path.join(__dirname, 'index.html'));
+    response.sendFile(path.join(__dirname, 'index.html'));
 })
 
 app.get("/getItems",(request,response)=>{
@@ -17,14 +23,26 @@ app.post('/addItem',(request,response)=>{
     response.send(arr);
     // console.log(temp);
 })
-app.put('updateItem',(request,response)=>{
+app.put('/updateItem',(request,response)=>{
     let temp = request.body;
     for(let i=0;i<arr.length;i++){
         if(arr[i].name == temp.name){
-            arr[i].name == temp.newName;
+            arr[i].name = temp.newName;
         }
         else{
         }
     }
-    response.send("Cập nhật thành công");
+    response.send(arr);
+})
+app.delete("/deleteItem",(resquest,response)=>{
+    let temp = resquest.body;
+    console.log(arr);
+
+    for(let i = 0; i<arr.length;i++){
+        if(arr[i].name == temp.name){
+            console.log(arr[i].name);
+           arr.splice(i,i+1);
+        }
+    }
+    response.send(arr);
 })
