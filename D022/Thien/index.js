06d9d8622a36e9eb4e435c5092aabeff0c652b52
ThanhThien269 <thien.mlt8601@sinhvien.hoasen.edu.vn>
2022-03-02 22:09:11 +0700

    D22 final

--- D022/Thien/index.js
+++ D022/Thien/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const bodyParser =require('body-parser');
-const { response } = require('express');
+const { response, request } = require('express');
+const { required } = require('nodemon/lib/config');
 const app =express();
-
+const path = require('path')
 app.use(bodyParser.json())
 let arr=[{name:"Thien"},{name:"Linh"}];
 app.listen('3000',(request,response)=>{
     console.log('Server đã chạy');
 })
-
+app.get('',(request,response)=>{
+    response.sendFile(path.join(__dirname,'index.html'))
+})
 app.get("/getItems",(request,response)=>{
     response.status(200).send(arr);
 })
@@ -19,13 +22,26 @@ app.post("/addItem",(resquest,response)=>{
 })
 
 app.put('/updateItem',(resquest,response)=>{
-    let newName = resquest.body;
-    for(let i =0;i,arr.length;i++){
-        if(arr[i].name==newName.name){
-            arr[i].name=newName.name;
+    let temp = resquest.body;
+    for(let i =0;i<arr.length;i++){
+        if(arr[i].name==temp.name){
+            arr[i].name=temp.newName;
         }else{
-            response.send("Không tìm thấy tên")
         }
-        response.send("Cập nhật thành công"+arr);
+       
     }
+    response.send(arr);
+})
+app.delete("/deleteItem",(resquest,response)=>{
+    let temp = resquest.body;
+    console.log(arr);
+
+    for(let i = 0; i<arr.length;i++){
+        if(arr[i].name=temp.name){
+            console.log(arr[i].name);
+            arr.splice(i,i+1);
+
+        }
+    }
+    response.send(arr);
 })
